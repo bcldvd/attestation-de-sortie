19a5efb08405595335fe55d1da8755acb5dfec82
David <david.bocle@finastra.com>
2020-10-30 13:11:03 +0100

    multi process

--- src/generate/generate.service.ts
+++ src/generate/generate.service.ts
@@ -24,7 +24,6 @@ export class GenerateService {
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--single-process',
       ],
     });
   }
