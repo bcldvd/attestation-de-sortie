4d53c451f78c006348b8a2d8d348329a69926fe3
David <david.bocle@finastra.com>
2020-10-30 18:23:25 +0100

    revert

--- src/generate/generate.service.ts
+++ src/generate/generate.service.ts
@@ -20,13 +20,7 @@ export class GenerateService {
 
   async init() {
     this.browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--single-process',
-      ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
   }
 
