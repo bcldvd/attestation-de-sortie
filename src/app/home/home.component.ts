4d12d7de2e6acb1c30a6d8ce1e76ece389458d4a
David <david.bocle@finastra.com>
2020-11-02 12:17:00 +0100

    routing based on OS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OS } from './home.enum';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  redirectToOS() {
+    const os = this.getMobileOperatingSystem();
+    console.log(os);
+    if (os === OS.unknown) {
+      this.router.navigate(['ios']);
+    } else {
+      this.router.navigate([os]);
+    }
   }
 
+  getMobileOperatingSystem() {
+    const userAgent = navigator.userAgent || navigator.vendor;
+
+    if (/android/i.test(userAgent)) {
+      return OS.Android;
+    }
+
+    if (/iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return OS.iOS;
+    }
+
+    return OS.unknown;
+  }
 }
---
+++ src/app/home/home.enum.ts
@@ -0,0 +1,5 @@
+export enum OS {
+  Android = 'android',
+  iOS = 'ios',
+  unknown = '',
+}
